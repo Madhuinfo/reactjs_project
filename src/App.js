351fb22ad58b1bfd12bfedeb02df5d202fe24531
Madhuinfo <104617952+Madhuinfo@users.noreply.github.com>
2022-05-04 18:03:12 +0530

    app.js file commit

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ function App() {
     <Greet name="Clark" work="Staff">
       <p>Hello Clark Nice to meet you</p>
     </Greet>
-    <Greet name="jack" work="Professer">
-      <button>Submit</button>
-    </Greet>
-    <Greet name="Simon" work="BusinessMan" />
+    <Greet name="jack" work="Professer" />
+      {/* <button>Submit</button> */}
+    {/* </Greet> */}
+    <Greet name="Simon" work="BusinessMan">
+    <p>Hello Clark Nice to meet you</p>
+      </Greet>
     <Greet name="vidal" work="Student" />
     </div>
   );
